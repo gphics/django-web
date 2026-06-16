9cae344281dce113b0ef02e7727aa00d37618176
gphics <abdulbasitabdulakeem234@gmail.com>
2026-06-16 22:43:40 +0100

    chai

--- finguard/server/core/settings.py
+++ finguard/server/core/settings.py
@@ -34,8 +34,8 @@ debug_env = True if os.environ.get("DEBUG")  == "Yes" else False
 DEBUG = debug_env
 
 # Read ALLOWED_HOSTS from Render environment variables, fallback to localhost for development
-ALLOWED_HOSTS = ["https://finguard-fvnu.onrender.com"]
-# ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+# ALLOWED_HOSTS = ["https://finguard-fvnu.onrender.com"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST")]
 
 
 # Application definition
